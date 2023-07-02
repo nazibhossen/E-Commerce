6b3a023d84c0672d711cbe225ae74166f10d143c
Nazib Hossen <37033448+nazibhossen@users.noreply.github.com>
2023-07-02 15:10:07 +0600

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -68,6 +68,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'deterministic': True,
+        },
     }
 }
 
